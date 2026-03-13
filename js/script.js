034b30b3a953c6509bb6d4b8843576839cc77895
sylvesternnachi <sylvelity009@yahoo.com>
2026-03-13 13:10:13 +0100

    Added other features like edit etc

--- js/script.js
+++ js/script.js
@@ -8,7 +8,12 @@ const priorityList = document.querySelectorAll('.priority-btn');
 const filterItem = document.querySelector('.filter-item');
 const filterInput = document.getElementById('filter-items')
 
+
 let selectedPriority = null;
+let itemId = null;
+let itemName = null;
+let isEditMode = false;
+let checkboxResult = null;
 
 const highItemList = document.getElementById('high-items-list');
 const mediumItemList = document.getElementById('medium-items-list');
@@ -115,7 +120,8 @@ function addItem(e){
         //  // Generate next ID
         const nextId = getLsItems.length > 0 ? Math.max(...getLsItems.map(itemList => itemList.id)) + 1
                                               :  1;
-        
+        //SetCheckbox defaul status to uncheck
+         checkboxResult = 'unchecked';
 
         // Start creating the Li
         const li = document.createElement('li');
@@ -157,7 +163,8 @@ function addItem(e){
         const inputPriorityItem = {
             id: nextId,
             item:  textInput.value,
-            priority: selectedPriority
+            priority: selectedPriority,
+            checkstatus: checkboxResult
         }
 
         //Add to LocalHost 
@@ -266,6 +273,11 @@ function addItem(e){
 
         });
 
+
+
+
+
+
        checkUI();
 
 
@@ -281,7 +293,8 @@ function checkUI(){
      const mediumList = mediumItemList.querySelectorAll('li');
      const lowList = lowItemList.querySelectorAll('li');
 
-   
+     // Join all the UI so I can use them for checkbox
+  const allLis = [...checkHighList, ...mediumList, ...lowList];
 
 
      //GlobalCheck to add empty State
@@ -343,7 +356,63 @@ function checkUI(){
                  selectList.classList.remove('medium');
                  selectList.classList.remove('low');
             });
-    }
+    }   
+
+
+        // Make sure check UI update the Lis with the item check from LS
+
+    const itemFromStorage = getItemFromLs();
+
+            let globalCheckBoxId = [];
+             lsCheckboxStatus = 'checked';
+
+             itemFromStorage.forEach(itemList => {
+
+                if(itemList.checkstatus === lsCheckboxStatus){
+                    globalCheckBoxId.push(itemList.id);
+                }
+                console.log(globalCheckBoxId);
+                   
+            });
+
+
+            allcheckbox = allLis.forEach(li => { 
+
+            liIds = li.getAttribute('data-id');
+
+                if(globalCheckBoxId.includes(Number(liIds))){
+                    
+                    const checkbox = li.querySelector('input[type="checkbox"]');
+                    checkbox.setAttribute('checked', lsCheckboxStatus);
+                    checkbox.checked = true;
+                     li.classList.add('strikethrough-text');
+
+                    console.log(globalCheckBoxId)
+                }
+
+         });
+
+       
+
+       
+
+
+
+ 
+    
+    // const itemFromStorage = getItemFromLs();
+
+    //   itemFromStorage.forEach(itemList => {
+    //         if(itemList.id === Number(liId))
+    //            itemList.checkstatus =  checkboxResult;
+    //     });
+
+    //         li.classList.add('strikethrough-text');
+    //         const checkbox = allLis.querySelector('input[type="checkbox"]');
+    //         checkbox.setAttribute('checked', checkboxResult);
+
+
+
 }
 
 
@@ -430,9 +499,149 @@ function removeItem(e){
 
 
 
+};
+
+//...........Edit Content //...................
+
+function editItem(e){
+
+    
+
+     const itemFromStorage = getItemFromLs();
+
+     //-------- THE PURPOSE FOR THIS CODE IS TO FADE THE LI COLOR THAT IS BEEN EDITTED ---
+
+     //Getting all the Lis for both High, Medium & Low
+    const checkHighList = highItemList.querySelectorAll('li');
+     const mediumList = mediumItemList.querySelectorAll('li');
+     const lowList = lowItemList.querySelectorAll('li');
+
+     // Join all the UI so I can use them for checkbox
+    const allLis = [...checkHighList, ...mediumList, ...lowList];
+    //--------------------------------------------------------------------------------------
+
+
+    if(e.target.classList.contains('item')){
+
+
+
+        //--- ACTUAL CODE TO FADE THE EDIT LI COLOR -----------
+        allLis.forEach(Lis => {
+            //Set the default color
+             Lis.style.color = '#444'
+
+             // Once it been click the active Lis choose a new color
+            if(Lis.getAttribute('data-id') === e.target.getAttribute('data-id')){
+                console.log(Lis);
+                Lis.style.color = '#ccc'
+            }
+        })
+
+        //-------------------------------------------------------------------------
+
+       
+        //EDIT FLOW CONTINUES HERE
+
+        textInput.value = e.target.innerText;
+        priorityShow.style.display = 'flex';
+    
+
+        isEditMode = true;
+
+         itemFromStorage.forEach(itemList => {
+       
+        if(itemList.item === e.target.innerText ){
+
+       
+         selectedPriority = itemList.priority;
+         itemId = itemList.id
+       
+        priorityList.forEach(selectList => {
+
+                     selectList.classList.remove('high','medium','low');
+
+                     if(selectedPriority === 'High' && selectList.classList.contains('1')){
+                        selectList.classList.add('high');
+                    }
+
+                    if(selectedPriority === 'Medium' && selectList.classList.contains('2')){
+                        selectList.classList.add('medium');
+                    }
+
+                    if(selectedPriority === 'Low' && selectList.classList.contains('3')){
+                        selectList.classList.add('low');
+                    }
+
+            //Add this to the global variable
+            selectedPriority = itemList.priority;
+
+         });
+
+
+         }
+
+
+        });
+
+    };
+    
+    
+    
 }
 
 //------------------------------------------------------------------
+
+function updateEdit(){
+
+    // Get updated values from the input & priority selection
+    let newText = textInput.value;
+    const newPriority = selectedPriority;
+    
+    if(newText.trimStart().length > 0 ) {
+        
+     let itemFromStorage = getItemFromLs();
+
+    itemFromStorage.forEach(itemList => {
+        if(itemList.id === Number(itemId)){
+           itemList.item = newText;        // <-- use input value
+            itemList.priority = newPriority; // <-- use selectedPriority
+        }
+    });
+    
+     localStorage.setItem('lsitems', JSON.stringify(itemFromStorage));
+
+
+        // setting back the edit mode to false;
+      isEditMode = false;
+
+    // Reset input field if you want
+    textInput.value = '';
+    selectedPriority = null;
+    itemId = null;
+
+ // Clear existing li from the displaty to lists
+   clearOldLists()
+
+    // Re-display items
+    displayItem();
+    checkUI();
+
+    } else{
+        alert('Enter a valid input');
+    }
+
+
+}
+
+//------ CLear list After Update or it show the new editted item ---
+
+function clearOldLists() {
+    highItemList.querySelectorAll('li').forEach(li => li.remove());
+    mediumItemList.querySelectorAll('li').forEach(li => li.remove());
+    lowItemList.querySelectorAll('li').forEach(li => li.remove());
+}
+
+//
 
 //RemoveItem from LocalStorage 
 
@@ -453,25 +662,64 @@ function removeItem(e){
 // TickAsDone
 
 function tickAsDone(e){
+
+     const itemFromStorage = getItemFromLs();
+
       if(e.target.classList.contains('checkbox')){
-    const li = e.target.parentElement;
+
+         const li = e.target.parentElement;
+         const liId = e.target.parentElement.getAttribute("data-id");
+
+         console.log(typeof liId);
+    // console.log(e.target.checked)
 
      if(e.target.checked){
+
+        checkboxResult = 'checked';
+
+         itemFromStorage.forEach(itemList => {
+            if(itemList.id === Number(liId))
+               itemList.checkstatus =  checkboxResult;
+        });
+
             li.classList.add('strikethrough-text');
+            const checkbox = li.querySelector('input[type="checkbox"]');
+            checkbox.setAttribute('checked', checkboxResult);
+            
         } else{
+
+                  checkboxResult = 'unchecked';
+
+                itemFromStorage.forEach(itemList => {
+                    if(itemList.id === Number(liId))
+                    itemList.checkstatus =  checkboxResult;
+                });
+
               li.classList.remove('strikethrough-text');
+
+            const checkbox = li.querySelector('input[type="checkbox"]');
+            checkbox.removeAttribute('checked');
         }
    }
    
+   localStorage.setItem('lsitems', JSON.stringify(itemFromStorage));
 }
 
 //Initialize App
 function init(){
 //Event Listener
-form.addEventListener('submit', addItem);
+form.addEventListener('submit', function (e) { 
+    e.preventDefault();
+    if(isEditMode){
+        updateEdit();
+    }else{
+        addItem(e);
+    }
+});
 mainBody.addEventListener('click', removeItem);
+mainBody.addEventListener('click', editItem);
 mainBody.addEventListener('change', tickAsDone );
-mainBody.addEventListener('change', tickAsDone );
+// mainBody.addEventListener('change', tickAsDone );
 filterInput.addEventListener('input', filterItemFunc);
 addEventListener('DOMContentLoaded', displayItem);
 
